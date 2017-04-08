ffc242ea436875563342be34a0f392e91f962bd9
Yash <puthranyash1@gmail.com>
2017-04-08 23:24:57 +0530

    Completed Messages

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io').listen(http);
+//
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+//
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+//   socket.on('disconnect', function(){
+//   console.log('user disconnected');
+// });
+//   socket.on('chat message', function(msg){
+//     console.log('message: ' + msg);
+//   });
+//
+//   socket.on('chat message', function(msg){
+//     io.emit('chat message', msg);
+//   });
+// });
+//
+//
+// http.listen(3000, function(){
+//   console.log('listening on *:3000');
+// });
 
-users = [];
-connections = [];
-
-server.listen(process.env.PORT || 3000);
-console.log("Server Started");
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function(socket){
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 
-//Connect
-  connections.push(socket);
-  console.log("Connected: %s sockets connected", connections.length);
-
-//Disconnect
-  socket.on("disconnect", function(data) {
-    connections.splice(connections.indexOf(socket),1);
-    console.log("Disconnected: %s sockets connected", connections.length);
-  })
-
-
-
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
