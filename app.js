fa7b77d0b398eb5c623c7f0a0bd8823f4295d20f
Yash <puthranyash1@gmail.com>
2017-04-09 18:54:37 +0530

    Added User Integration

--- app.js
+++ app.js
@@ -2,18 +2,52 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
+var users = [];
+var connections = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+io.sockets.on('connection', function(socket){
+
+    connections.push(socket);
+    console.log("Connected: %s sockets connected", connections.length);
+
+    socket.on('new user', function(data, callback){
+      if(users.indexOf(data) != -1){
+        callback(false);
+      }
+      else{
+        callback(true);
+        socket.user = data;
+        users.push(socket.user);
+        updateUsers();
+      }
+    });
+
+    socket.on('chat message', function(msg){
+    io.emit('chat message', {msg: msg, user: socket.user});
+  });
+
+  function updateUsers(){
+    io.sockets.emit('usernames', users);
+  }
+
+  socket.on('disconnect', function(data){
+    if(!socket.user){
+      return;
+    }
+    else{
+      connections.pop();
+      console.log("Disconnected: %s connected", connections.length);
+      users.splice(users.indexOf(socket.user),1);
+      updateUsers();
+    }
   });
 });
 
 http.listen(port, function(){
-  console.log('listening on *:' + port);
+  console.log('Server started on port ' + port);
 });
